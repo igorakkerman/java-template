3ba5049578bbc4b2041982b7514794c174fe3ea1
Igor Akkerman <commit@igorakkerman.de>
2019-11-02 23:04:25 +0100

    Exception test case added

--- src/main/java/de/igorakkerman/kata/xyz/Xyz.java
+++ src/main/java/de/igorakkerman/kata/xyz/Xyz.java
@@ -2,6 +2,9 @@ package de.igorakkerman.kata.xyz;
 
 public class Xyz {
     public int process(int input) {
+        if (input < 0)
+            throw new IllegalArgumentException("think positive");
+
         return 0;
     }
 }
--- src/test/java/de/igorakkerman/kata/xyz/XyzTest.java
+++ src/test/java/de/igorakkerman/kata/xyz/XyzTest.java
@@ -3,19 +3,25 @@ package de.igorakkerman.kata.xyz;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class XyzTest {
 
     @Test
     void zero() {
-        assertThat(new Xyz().process(0))
-                .isZero();
+        assertThat(new Xyz().process(0)).isZero();
     }
 
     @Test
     void one() {
         assertThat(new Xyz().process(1))
                 .isZero();
+    }
+
+    @Test
+    void minus1() {
+        assertThatThrownBy(() -> new Xyz().process(-1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("think positive");
     }
 }
